5720af4d1b9efbf3af28c24569aba477c788c021
kanadOncode-141 <kbbiswas01.hl@gmail.com>
2022-12-14 23:00:50 +0530

    Pricing day 1

---
+++ mailchimp/src/pages/Pricing/Price_component/Market.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+const Market = () => {
+  return (
+    <div>Market</div>
+  )
+}
+
+export default Market;
---
+++ mailchimp/src/pages/Pricing/Price_component/Website.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Website = () => {
+  return (
+    <div>Website</div>
+  )
+}
+
+export default Website;
