2bb688fac65fbee468a1fa18956fd70a5c176ecf
shivamsherje <sherjes49@gmail.com>
2022-12-14 21:59:45 +0530

    changes in app.js

--- mailchimp/src/App.js
+++ mailchimp/src/App.js
@@ -1,10 +1,15 @@
-import './App.css';
-
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import MainPage from "./pages/Homepage/MainPage";
+import Footer from "./components/Footer/Footer.tsx"
 function App() {
-  
+
   return (
+
     <div className="App">
-   
+      <Navbar/>
+      <MainPage />
+      <Footer/>
     </div>
   );
 }
