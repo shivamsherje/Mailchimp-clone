34ce6089a0916194ba0d42a906bc15f49b1adf4c
kanadOncode-141 <kbbiswas01.hl@gmail.com>
2022-12-15 22:21:01 +0530

    Pricing day3

--- mailchimp/src/pages/Pricing/Pricing.jsx
+++ mailchimp/src/pages/Pricing/Pricing.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
+
+import { Link } from 'react-router-dom';
 import Market from './Price_component/Market';
-import Transactional from './Price_component/Transactional';
-import Website from './Price_component/Website';
+
 
 const Pricing = () => {
   return (
     <div>
         <Market/>
-        <Website/>
-        <Transactional/>
     </div>
   )
 }
