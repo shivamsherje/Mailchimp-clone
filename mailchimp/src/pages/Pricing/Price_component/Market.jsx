e7c6aeb752a85ac8410000f79c9c8b5a4cd4eec7
kanadOncode-141 <kbbiswas01.hl@gmail.com>
2022-12-17 00:09:34 +0530

    pricing day 4

--- mailchimp/src/pages/Pricing/Price_component/Market.jsx
+++ mailchimp/src/pages/Pricing/Price_component/Market.jsx
@@ -1,3 +1,4 @@
+import { Button } from '@chakra-ui/react';
 import React from 'react'
 import "./MarketStyle.css";
 
@@ -36,33 +37,249 @@ const Market = () => {
         
         </div>
 
-        <div className='price_table'>
-            <div>
-                <p>Monthly Email Sends</p>
-                <p>Users</p>
-                <p>Customer Support</p>
-                <p>Pre-built Email Templates</p>
-                <p>300+ Integrations</p>
-                <p>Basic Reporting & Analytics</p>
-                <p>forms & landing page</p>
+
+{/* Price table */}
+
+
+    <div className='price_table'>
+        <div className='lis'>
+
+                <span className='first'>Monthly Email Sends</span>
+                <span>Users</span>
+                <span>Customer Support</span>
+                <span>Pre-built Email Templates</span>
+                <span>300+ Integrations</span>
+                <span>Basic Reporting & Analytics</span>
+                <span>forms & landing page</span>
+                <span>Creative Assistant</span>
+                <span>Role-based Access</span>
+                <span>Remove Mailchimp Branding</span>
+                <span>A/B Testing</span>
+                <span>Email Scheduling</span>
+                <span>Automated Customer Journeys</span>
+                <span>Pre-built Journeys</span>
+                <span>Custom-Coded Templates</span>
+                <span>Predictive Segmentation</span>
+                <span>Content Optimizer</span>
+                <span>Send Time Optimization</span>
+                <span>Dynamic Content</span>
+                <span>Multivariate Testing</span>
+                <span>Comparative Reporting</span>
+                <span>Advanced Segmentation</span>
+                <span>Assisted Onboarding</span>
             </div>
+
+
+
+
             <div className='border_price'>            
-                <div>
-                    <></>
+
+
+
+                {/* Premium  pacakage */}
+                <div className='pirce_box'>
+                    <p  className='recommend_hid'>Mailchimp Recommends</p> 
+                    <div className='stan1'>
+                        <h3>Premium</h3>
+                        <p>Our most advanced tools, unlimited contacts, and priority support; built for teams.</p>
+                        
+                        <div>
+                            <p>Starts at</p>
+                            <div className='convert'>
+                                &#x00024;<h2>23,000</h2>
+                            </div>
+                            <p>/month*</p>
+                        </div>
+
+
+                        <Button>Buy Now</Button>
+                        
+                        <div>
+                            <p>*<u>Overages</u> apply if contact or email send limit is exceeded.</p>
+                            <span>Learn more</span>
+                        </div>
+
+                        <p>150,000</p>
+                        <p>Unlimited</p>
+                        <p>Phone & Priority Support</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>5 Roles</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>Enhanced</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                    </div>  
                 </div>
-                <div>
-                    <></>
+
+
+
+                {/* Standerd package */}
+                <div className='standard_box'>
+                    <p className='recommend'>Mailchimp Recommends</p> 
+                    <div className='stan' >
+                        <h3>Premium</h3>
+                        <p>Our most advanced tools, unlimited contacts, and priority support; built for teams.</p>
+                        
+                        <div>
+                            <p>Starts at</p>
+                            <div className='convert'>
+                                &#x00024;<h2>23,000</h2>
+                            </div>
+                            <p>/month*</p>
+                        </div>
+
+
+                        <Button>Buy Now</Button>
+
+                        <div>
+                            <p>*<u>Overages</u> apply if contact or email send limit is exceeded.</p>
+                            <span>Learn more</span>
+                        </div>
+
+                        <p>150,000</p>
+                        <p>Unlimited</p>
+                        <p>Phone & Priority Support</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>5 Roles</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>Enhanced</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                    </div>
                 </div>
-                <div>
-                    <></>
+
+
+
+                {/* Essentials package */}
+                <div className='pirce_box'>
+                    <p  className='recommend_hid'>Mailchimp Recommends</p>
+                    <div className='stan1'>
+                        <h3>Premium</h3>
+                        <p>Our most advanced tools, unlimited contacts, and priority support; built for teams.</p>
+                        
+                        <div>
+                            <p>Starts at</p>
+                            <div className='convert'>
+                                &#x00024;<h2>23,000</h2>
+                            </div>
+                            <p>/month*</p>
+                        </div>
+
+                        <Button>Buy Now</Button>
+
+                        <div>
+                            <p>*<u>Overages</u> apply if contact or email send limit is exceeded.</p>
+                            <span>Learn more</span>
+                        </div>
+
+
+                        <p>150,000</p>
+                        <p>Unlimited</p>
+                        <p>Phone & Priority Support</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>5 Roles</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>Enhanced</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                    </div>
                 </div>
-                <div>
-                    <></>
+
+
+                
+                {/* Free Pacakge */}
+                <div className='pirce_box2'>
+                    <p  className='recommend_hid'>Mailchimp Recommends</p>
+                    <div className='stan1'>
+                        <h3>Premium</h3>
+                        <p>Our most advanced tools, unlimited contacts, and priority support; built for teams.</p>
+
+                        <div>
+                            <p>Starts at</p>
+                            <div className='convert'>
+                                &#x00024;<h2>23,000</h2>
+                            </div>
+                            <p>/month*</p>
+                        </div>
+
+                        <Button>Buy Now</Button>
+
+                        <div>
+                            <p>*<u>Overages</u> apply if contact or email send limit is exceeded.</p>
+                            <span>Learn more</span>
+                        </div>
+
+
+                        <p>150,000</p>
+                        <p>Unlimited</p>
+                        <p>Phone & Priority Support</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>5 Roles</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>Enhanced</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                        <p>&#x2713;</p>
+                    </div>    
                 </div>
+
             </div>
         </div>
-
-
     </div>
   )
 }
