46c8c316251a7bd5272223c4d87ed2d6dbfc8deb
shivamsherje <sherjes49@gmail.com>
2022-12-17 16:31:32 +0530

    changes in main

--- mailchimp/src/App.js
+++ mailchimp/src/App.js
@@ -3,14 +3,14 @@ import Navbar from "./components/Navbar/Navbar";
 import MainPage from "./pages/Homepage/MainPage";
 import Footer from "./components/Footer/Footer.tsx";
 import Integrations from "./pages/Homepage/integrations";
-// import Products from "./pages/Products/Products";
+import Products from "./pages/Products/Products";
 function App() {
   return (
     <div className="App">
       <Navbar />
       <MainPage />
       <Integrations/>
-      {/* <Products /> */}
+      <Products />
       <Footer />
     </div>
   );
